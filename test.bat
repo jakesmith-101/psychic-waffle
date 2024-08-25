7baf029ddc06dfe9b348514195e59655c802b406
ZiarayZ <jakesmith@binaryrage.com>
2024-08-25 14:14:28 +0100

    tweak

--- test.bat
+++ test.bat
@@ -1,12 +1,13 @@
 REM creates pg_data directory for postgresdb volume
-if exist .\pg_data\NUL echo "Folder already exists"
-if not exist .\pg_data\NUL MKDIR "pg_data"
+if exist pg_data\NUL echo "Folder already exists"
+if not exist pg_data\NUL MKDIR "pg_data"
 
 REM creates pg_data_test directory for postgres_test volume
-if exist .\pg_data_test\NUL echo "Folder already exists"
-if not exist .\pg_data_test\NUL MKDIR "pg_data_test"
+if exist pg_data_test\NUL echo "Folder already exists"
+if not exist pg_data_test\NUL MKDIR "pg_data_test"
 
 REM auto generate env vars
+if exist .env echo "Env already exists"
 if not exist .env (
     echo "# Postgres"
     echo "DB_HOST=postgresdb"
