571edaa391271b0555deb3dda6a9dd2cc49e751e
ZiarayZ <jakesmith@binaryrage.com>
2024-08-25 14:11:48 +0100

    create env file

--- test.bat
+++ test.bat
@@ -6,4 +6,21 @@ REM creates pg_data_test directory for postgres_test volume
 if exist .\pg_data_test\NUL echo "Folder already exists"
 if not exist .\pg_data_test\NUL MKDIR "pg_data_test"
 
+REM auto generate env vars
+if not exist .env (
+    echo "# Postgres"
+    echo "DB_HOST=postgresdb"
+    echo "DB_DRIVER=postgres"
+    echo "DB_USER=spuser"
+    echo "DB_PASSWORD=SPuser"
+    echo "DB_NAME=psychic-waffle"
+    echo "DB_PORT=5432"
 
+    echo "# Postgres Test"
+    echo "TEST_DB_HOST=postgres_test"
+    echo "TEST_DB_DRIVER=postgres"
+    echo "TEST_DB_USER=spuser"
+    echo "TEST_DB_PASSWORD=SPuser_test"
+    echo "TEST_DB_NAME=psychic-waffle_test"
+    echo "TEST_DB_PORT=5432"
+) > .env
