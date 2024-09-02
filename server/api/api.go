84db1f0ffd42183248d36afce064fb31f92a4493
ZiarayZ <jakesmith@binaryrage.com>
2024-09-02 11:49:17 +0100

    camelCase - need to idiot proof BuildPath

--- server/api/api.go
+++ server/api/api.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 )
 
-var ApiVer = "v1"
-var RootPath = fmt.Sprintf("/api/%s", ApiVer)
+var apiVer = "v1"
+var rootPath = fmt.Sprintf("/api/%s", apiVer)
 
 func BuildPath(path string) string {
-	return fmt.Sprintf("%s%s", RootPath, path)
+	return fmt.Sprintf("%s%s", rootPath, path)
 }
