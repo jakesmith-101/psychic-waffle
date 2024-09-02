92ff7f1e5799849ed63d975a78be354659c9f2e7
ZiarayZ <jakesmith@binaryrage.com>
2024-09-02 16:10:39 +0100

    form elements

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -1,1 +1,15 @@
+<script lang="ts">
+	let showPassword = false;
+	$: password = showPassword ? 'text' : 'password';
+</script>
+
 <h1>Welcome to login</h1>
+
+<div>
+	<label for="username" class="sr-only">Username</label>
+	<input id="username" name="username" type="text" required placeholder="Username" />
+</div>
+<div>
+	<label for="password" class="sr-only">Password</label>
+	<input id="password" name="password" type={password} required placeholder="Password" />
+</div>
--- client/src/routes/signup/+page.svelte
+++ client/src/routes/signup/+page.svelte
@@ -1,1 +1,23 @@
+<script lang="ts">
+	let showPassword = false;
+	$: password = showPassword ? 'text' : 'password';
+</script>
+
 <h1>Welcome to signup</h1>
+
+<div>
+	<label for="username">Username</label>
+	<input id="username" name="username" type="text" required placeholder="Username" />
+</div>
+<div>
+	<label for="password">Password</label>
+	<input id="password" name="password" type={password} required placeholder="Password" />
+</div>
+<div>
+	<label for="password">Confirm Password</label>
+	<input id="password" name="password" type={password} required placeholder="Password" />
+</div>
+<div>
+	<input type="checkbox" bind:checked={showPassword} id="showPassword" />
+	<label for="showPassword"> Show Password </label>
+</div>
