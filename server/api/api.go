16707b26b36c96130a6a674abdec9af41e5ba029
ZiarayZ <jakesmith@binaryrage.com>
2024-09-02 11:50:39 +0100

    idiot proof join

--- server/api/api.go
+++ server/api/api.go
@@ -2,11 +2,12 @@ package api
 
 import (
 	"fmt"
+	"path"
 )
 
 var apiVer = "v1"
 var rootPath = fmt.Sprintf("/api/%s", apiVer)
 
-func BuildPath(path string) string {
-	return fmt.Sprintf("%s%s", rootPath, path)
+func BuildPath(route string) string {
+	return path.Join(rootPath, route)
 }
