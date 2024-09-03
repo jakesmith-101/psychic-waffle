4214c798c1fa253e64217fa271916bfdcef82574
ZiarayZ <jakesmith@binaryrage.com>
2024-09-03 10:41:04 +0100

    linting

--- client/src/routes/login/+page.server.ts
+++ client/src/routes/login/+page.server.ts
@@ -1,9 +1,9 @@
 import type { Actions } from '@sveltejs/kit';
 
 export const actions: Actions = {
-    default: async ({ cookies, request }) => {
-        const data = await request.formData();
-        data.get('username');
-        data.get('password');
-    }
+	default: async ({ cookies, request }) => {
+		const data = await request.formData();
+		data.get('username');
+		data.get('password');
+	}
 };
--- client/src/routes/signup/+page.server.ts
+++ client/src/routes/signup/+page.server.ts
@@ -1,10 +1,10 @@
 import type { Actions } from '@sveltejs/kit';
 
 export const actions: Actions = {
-    default: async ({ cookies, request }) => {
-        const data = await request.formData();
-        data.get('username');
-        data.get('password');
-        data.get('confirmPassword');
-    }
+	default: async ({ cookies, request }) => {
+		const data = await request.formData();
+		data.get('username');
+		data.get('password');
+		data.get('confirmPassword');
+	}
 };
