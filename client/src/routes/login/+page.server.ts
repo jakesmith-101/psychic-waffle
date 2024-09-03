e90379f663e34cfcda824e87ae7fee892d103144
ZiarayZ <jakesmith@binaryrage.com>
2024-09-03 14:23:44 +0100

    make forms "functional" - linting

--- client/src/routes/login/+page.server.ts
+++ client/src/routes/login/+page.server.ts
@@ -1,9 +1,11 @@
+import * as api from '$lib/server/api.js';
 import type { Actions } from '@sveltejs/kit';
 
 export const actions: Actions = {
 	default: async ({ cookies, request }) => {
 		const data = await request.formData();
-		data.get('username');
-		data.get('password');
+		const username = data.get('username')?.toString();
+		const password = data.get('password')?.toString();
+		if (username !== undefined && password !== undefined) api.login(username, password);
 	}
 };
--- client/src/routes/signup/+page.server.ts
+++ client/src/routes/signup/+page.server.ts
@@ -1,10 +1,13 @@
+import * as api from '$lib/server/api.js';
 import type { Actions } from '@sveltejs/kit';
 
 export const actions: Actions = {
 	default: async ({ cookies, request }) => {
 		const data = await request.formData();
-		data.get('username');
-		data.get('password');
-		data.get('confirmPassword');
+		const username = data.get('username')?.toString();
+		const password = data.get('password')?.toString();
+		const confirmPassword = data.get('confirmPassword')?.toString();
+		if (username !== undefined && password !== undefined && password === confirmPassword)
+			api.signup(username, password, confirmPassword);
 	}
 };
