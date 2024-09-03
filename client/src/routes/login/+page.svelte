3b2ac6b7c79816b70414fac091c5f56851ff3823
ZiarayZ <jakesmith@binaryrage.com>
2024-09-03 10:18:26 +0100

    convert to forms

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -5,11 +5,13 @@
 
 <h1>Welcome to login</h1>
 
-<div>
-	<label for="username" class="sr-only">Username</label>
-	<input id="username" name="username" type="text" required placeholder="Username" />
-</div>
-<div>
-	<label for="password" class="sr-only">Password</label>
-	<input id="password" name="password" type={password} required placeholder="Password" />
-</div>
+<form method="post">
+	<div>
+		<label for="username" class="sr-only">Username</label>
+		<input id="username" name="username" type="text" required placeholder="Username" />
+	</div>
+	<div>
+		<label for="password" class="sr-only">Password</label>
+		<input id="password" name="password" type={password} required placeholder="Password" />
+	</div>
+</form>
--- client/src/routes/signup/+page.svelte
+++ client/src/routes/signup/+page.svelte
@@ -5,19 +5,21 @@
 
 <h1>Welcome to signup</h1>
 
-<div>
-	<label for="username">Username</label>
-	<input id="username" name="username" type="text" required placeholder="Username" />
-</div>
-<div>
-	<label for="password">Password</label>
-	<input id="password" name="password" type={password} required placeholder="Password" />
-</div>
-<div>
-	<label for="password">Confirm Password</label>
-	<input id="password" name="password" type={password} required placeholder="Password" />
-</div>
-<div>
-	<input type="checkbox" bind:checked={showPassword} id="showPassword" />
-	<label for="showPassword"> Show Password </label>
-</div>
+<form method="post">
+	<div>
+		<label for="username">Username</label>
+		<input id="username" name="username" type="text" required placeholder="Username" />
+	</div>
+	<div>
+		<label for="password">Password</label>
+		<input id="password" name="password" type={password} required placeholder="Password" />
+	</div>
+	<div>
+		<label for="password">Confirm Password</label>
+		<input id="password" name="password" type={password} required placeholder="Password" />
+	</div>
+	<div>
+		<input type="checkbox" bind:checked={showPassword} id="showPassword" />
+		<label for="showPassword"> Show Password </label>
+	</div>
+</form>
