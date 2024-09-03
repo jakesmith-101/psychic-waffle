4a974077f09309159e6fdc5e872ddb485a827b39
ZiarayZ <jakesmith@binaryrage.com>
2024-09-03 11:42:41 +0100

    basic auth code

---
+++ client/src/lib/server/database.ts
@@ -0,0 +1,11 @@
+export function login(username: string, password: string) {
+    if (username === '')
+        throw new Error('Missing Username');
+}
+
+export function signup(username: string, password: string, confirmPassword: string) {
+    if (username === '')
+        throw new Error('Missing Username');
+    if (password !== confirmPassword)
+        throw new Error('Password does not match');
+}
