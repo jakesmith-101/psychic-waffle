a58d9a265ac5313992376c608f48e0ed5b063aeb
ZiarayZ <jakesmith@binaryrage.com>
2024-09-08 18:27:59 +0100

    dedupe auth

--- client/src/lib/server/auth.ts
+++ client/src/lib/server/auth.ts
@@ -2,20 +2,18 @@ import { redirect } from '@sveltejs/kit';
 import { apiFetch } from './api';
 
 export async function login(username: string, passwordHash: string) {
-    if (username === '') throw new Error('Missing Username');
-    if (passwordHash === '') throw new Error('Missing Password');
-
-    const data = await apiFetch('/auth/login', 'POST', { username, passwordHash });
-    // FIXME: data holds user id and jwt token
-
-    throw redirect(303, `/dashboard`);
+    return auth('login', username, passwordHash);
 }
 
 export async function signup(username: string, passwordHash: string) {
+    return auth('signup', username, passwordHash);
+}
+
+async function auth(path: string, username: string, passwordHash: string) {
     if (username === '') throw new Error('Missing Username');
     if (passwordHash === '') throw new Error('Missing Password');
 
-    const data = await apiFetch('/auth/signup', 'POST', { username, passwordHash });
+    const data = await apiFetch(`/auth/${path}`, 'POST', { username, passwordHash });
     // FIXME: data holds user id and jwt token
 
     throw redirect(303, `/dashboard`);
