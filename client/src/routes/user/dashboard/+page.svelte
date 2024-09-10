3e96d0895c18b9fca08090e3523b9517a22b787c
ZiarayZ <jakesmith@binaryrage.com>
2024-09-10 15:53:50 +0100

    styling dashboard options

--- client/src/routes/user/dashboard/+page.svelte
+++ client/src/routes/user/dashboard/+page.svelte
@@ -4,5 +4,30 @@
     export let data: PageData;
 </script>
 
-<a href="/user/dashboard/settings">User Settings</a>
-<a href={`/user/${data.UserID}`}>User Details</a>
+<ul>
+    <li><a href="/user/dashboard/settings">User Settings</a></li>
+    <li><a href={`/user/${data.UserID}`}>User Details</a></li>
+</ul>
+
+<style>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        width: 200px;
+        background-color: #f1f1f1;
+    }
+
+    li a {
+        display: block;
+        color: #000;
+        padding: 8px 16px;
+        text-decoration: none;
+    }
+
+    /* Change the link color on hover */
+    li a:hover {
+        background-color: #555;
+        color: white;
+    }
+</style>
