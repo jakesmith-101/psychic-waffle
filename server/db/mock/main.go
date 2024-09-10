6a7edb1af1277dc61d209c43d111f6e4f8d12e03
ZiarayZ <jakesmith@binaryrage.com>
2024-09-10 09:33:20 +0100

    first error return - but call all funcs

--- server/db/mock/main.go
+++ server/db/mock/main.go
@@ -1,26 +1,18 @@
 package mock
 
-import (
-	"fmt"
-	"os"
-)
-
 func MockAll() error {
 	err := CreateRoleTable()
+	err2 := MockRoles()
+	err3 := CreateUserTable()
+	err4 := MockAdmin()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%e\n", err)
-	}
-	err = MockRoles()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%e\n", err)
-	}
-	err = CreateUserTable()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%e\n", err)
-	}
-	err = MockAdmin()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%e\n", err)
+		return err
+	} else if err2 != nil {
+		return err2
+	} else if err3 != nil {
+		return err3
+	} else if err4 != nil {
+		return err4
 	}
 	return err
 }
