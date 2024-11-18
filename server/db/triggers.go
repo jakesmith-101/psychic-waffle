c12dcfb1feb2db26cbedf239d62ce1a80769d1bb
ZiarayZ <jakesmith@binaryrage.com>
2024-11-18 16:36:23 +0000

    fix unique slug trigger/function

--- server/db/triggers.go
+++ server/db/triggers.go
@@ -6,7 +6,7 @@ func PostFuncs() error {
 	var err error
 
 	_, err = PgxPool.Exec(context.Background(), `
-	CREATE OR REPLACE FUNCTION public.set_slug_from_name() RETURNS trigger
+	CREATE OR REPLACE FUNCTION public.set_unique_slug() RETURNS trigger
 		LANGUAGE plpgsql
 		AS $$
 	DECLARE
@@ -14,13 +14,13 @@ func PostFuncs() error {
 		final_slug TEXT;
 		counter INTEGER := 1;
 	BEGIN
-		base_slug := NEW.slug;
+		base_slug := NEW.Slug;
 		final_slug := base_slug;
 
 		-- Loop to ensure uniqueness of the slug
 		LOOP
 			-- Check if the slug already exists in the table
-			IF EXISTS (SELECT 1 FROM "my_table_name" WHERE slug = final_slug AND id != COALESCE(NEW.id, 0)) THEN
+			IF EXISTS (SELECT 1 FROM "posts" WHERE Slug = final_slug AND PostID != COALESCE(NEW.PostID, 0)) THEN
 				-- If it exists, append a numeric suffix and increment the counter
 				final_slug := base_slug || '-' || counter;
 				counter := counter + 1;
@@ -30,8 +30,8 @@ func PostFuncs() error {
 			END IF;
 		END LOOP;
 
-		-- Set the unique slug to the 'slug' field of the NEW record
-		NEW.slug := final_slug;
+		-- Set the unique slug to the 'Slug' field of the NEW record
+		NEW.Slug := final_slug;
 		RETURN NEW;
 	END
 	$$;
@@ -41,11 +41,11 @@ func PostFuncs() error {
 	}
 
 	_, err = PgxPool.Exec(context.Background(), `
-	CREATE TRIGGER set_slug_from_name
+	CREATE TRIGGER set_unique_slug
 	BEFORE INSERT OR UPDATE
 	ON "posts"
 	FOR EACH ROW
-	EXECUTE FUNCTION public.set_slug_from_name();
+	EXECUTE FUNCTION public.set_unique_slug();
 	`)
 	if err != nil {
 		return err
