a600c72638e476ef4e7872c87121aee16a33584f
ZiarayZ <jakesmith@binaryrage.com>
2024-11-20 14:37:55 +0000

    move small chance code into main

--- server/db/mock/main.go
+++ server/db/mock/main.go
@@ -1,5 +1,7 @@
 package mock
 
+import "github.com/ZeFort/chance"
+
 func MockAll(mock bool) error {
 	var err error
 	var users []string
@@ -22,8 +24,7 @@ func MockAll(mock bool) error {
 		return err
 	}
 	if mock {
-		var userID string
-		userID, err = MockAdmin()
+		userID, err := MockAdmin()
 		if err != nil {
 			return err
 		}
@@ -49,4 +50,16 @@ func MockAll(mock bool) error {
 	}
 
 	return nil
+}
+
+var C *chance.Chance
+var cIsSet = false
+var cSeed int64
+
+func NewChance(seed int64) {
+	if !cIsSet || seed != cSeed {
+		C = chance.NewS(seed)
+		cIsSet = true
+		cSeed = seed
+	}
 }
