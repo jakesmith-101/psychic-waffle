29a6dfa1fa049f1f5de476555ebeb260718e24b2
ZiarayZ <jakesmith@binaryrage.com>
2024-11-29 15:39:25 +0000

    undo caching solution - doesn't work with Promise.all - will need to think on this more

--- client/src/routes/+page.server.ts
+++ client/src/routes/+page.server.ts
@@ -7,31 +7,13 @@ type tPost = Omit<tGetPosts["posts"][0], "authorID"> & {
         role: tGetRole
     }
 }
-type tCache<T> = { [k: string]: T };
 
 export async function load(): Promise<{ posts: tPost[] }> {
-    const roles: tCache<tGetRole> = {};
-    const users: tCache<tGetUser> = {};
     const data = await getPosts(true);
     const posts = await Promise.all(data.posts.map(async post => {
-        // attempting to cache each user to prevent repeated api calls for the same info
-        let user: tGetUser | undefined = undefined;
-        if (Object.keys(users).includes(post.authorID))
-            user = users?.[post.authorID];
-        else {
-            user = await getUser(post.authorID);
-            users[post.authorID] = user;
-        }
+        const user = await getUser(post.authorID);
         const { roleID, ...newUser } = user;
-
-        // attempting to cache each role to prevent repeated api calls for the same info
-        let role: tGetRole | undefined = undefined;
-        if (Object.keys(roles).includes(roleID))
-            role = roles?.[roleID];
-        else {
-            role = await getRole(roleID);
-            roles[roleID] = role;
-        }
+        const role = await getRole(roleID);
         const { authorID, ...newPost } = post;
 
         return {
