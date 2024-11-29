6b1d0b843d752ad90fc1679bb0616efd6d38b371
ZiarayZ <jakesmith@binaryrage.com>
2024-11-29 14:41:52 +0000

    reduce api calls made from loading a series of posts

--- client/src/routes/+page.server.ts
+++ client/src/routes/+page.server.ts
@@ -7,13 +7,31 @@ type tPost = Omit<tGetPosts["posts"][0], "authorID"> & {
         role: tGetRole
     }
 }
+type tCache<T> = { [k: string]: T };
 
 export async function load(): Promise<{ posts: tPost[] }> {
+    const roles: tCache<tGetRole> = {};
+    const users: tCache<tGetUser> = {};
     const data = await getPosts(true);
     const posts = await Promise.all(data.posts.map(async post => {
-        const user = await getUser(post.authorID);
+        // attempting to cache each user to prevent repeated api calls for the same info
+        let user: tGetUser | undefined = undefined;
+        if (post.authorID in Object.entries(users))
+            user = users?.[post.authorID];
+        else {
+            user = await getUser(post.authorID);
+            users[post.authorID] = user;
+        }
         const { roleID, ...newUser } = user;
-        const role = await getRole(roleID);
+
+        // attempting to cache each role to prevent repeated api calls for the same info
+        let role: tGetRole | undefined = undefined;
+        if (roleID in Object.entries(roles))
+            role = roles?.[roleID];
+        else {
+            role = await getRole(roleID);
+            roles[roleID] = role;
+        }
         const { authorID, ...newPost } = post;
 
         return {
